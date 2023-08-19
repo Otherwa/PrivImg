da41c78f26fe3e81173044bdc483ce1af6e85484
Otherwa <67428572+Atharv2002@users.noreply.github.com>
2023-08-19 13:27:06 +0530

    /minor fixes/

--- config/auth.js
+++ config/auth.js
@@ -2,8 +2,7 @@ const jwt = require('jsonwebtoken');
 require('dotenv').config()
 const verifyToken = (req, res, next) => {
     const token = req.cookies.token || ''; // Get the token from the cookie or header as needed
-    const user = req.session.user || '';
-    if (!token || !user) {
+    if (!token) {
         return res.redirect('/user/auth/login'); // Redirect to login if no token is present
     }
 
